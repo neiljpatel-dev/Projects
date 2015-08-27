e8136e29cd21c3a8ef70592cc194050a937f7bbd
prit280 <prit280@gmail.com>
2015-08-27 12:58:57 -0700

    Initial Commit

---
+++ Bouncing Labels/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bouncing Labels
+//
+//  Created by Pritam Kulkarni on 8/27/15.
+//  Copyright (c) 2015 Pritam Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
